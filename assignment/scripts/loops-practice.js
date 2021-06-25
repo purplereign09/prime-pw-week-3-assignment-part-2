8470a82dfacf6d66849b2a97978ca89ae2baf798
Patty Baraibar <pattybaraibar@msn.com>
2021-06-24 19:49:00 -0500

    question 3a finalized

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -70,8 +70,8 @@ while(j <= 5) {
 
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
-console.log('count backwards from 10 to 5');
+//console.log('count backwards from 10 to 5');
 let l = 10;
-while(10 >= 5){
-  console.log((l) -= 5);
+while(10 > 4){
+  console.log(l--);
 }
